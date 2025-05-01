fc0a19a580b30d622e83b56880707d493a11e7da
Miyuru Gunarathna <miyurupriyawadan@gmail.com>
2025-05-02 01:08:35 +0530

    feat: added next mf to core remote

--- apps/core/next.config.js
+++ apps/core/next.config.js
@@ -1,7 +1,24 @@
 // @ts-check
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config) {
+    const federationConfig = {
+      name: 'core',
+      filename: 'static/chunks/remoteEntry.js',
+      exposes: {
+        './modules/header': './src/modules/header/index.tsx',
+      },
+      shared: {},
+    };
+
+    config.plugins.push(
+      // @ts-ignore
+      new NextFederationPlugin(federationConfig),
+    );
+    return config;
+  },
   transpilePackages: ['@internal/ui'],
 };
 
