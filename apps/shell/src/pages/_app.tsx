c806c9a79887594316be87bc7ddb1839f875c486
Miyuru Gunarathna <miyurupriyawadan@gmail.com>
2025-05-01 22:01:05 +0530

    chore: format the code

--- apps/shell/src/pages/_app.tsx
+++ apps/shell/src/pages/_app.tsx
@@ -3,16 +3,16 @@ import './globals.css';
 import type { AppProps } from 'next/app';
 
 function CustomApp({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<Head>
-				<title>Micro Frontend with Module Federation</title>
-			</Head>
-			<main>
-				<Component {...pageProps} />
-			</main>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Micro Frontend with Module Federation</title>
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
 
 export default CustomApp;
--- apps/shell/src/pages/index.tsx
+++ apps/shell/src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
-	return (
-		<div>
-			<h1>Hola</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Hola</h1>
+    </div>
+  );
 }
