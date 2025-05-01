7802b78a201bfedbf0af0485b990efffb6988624
Miyuru Gunarathna <miyurupriyawadan@gmail.com>
2025-05-01 21:59:17 +0530

    feat: added tailwind configs

---
+++ apps/shell/src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import './globals.css';
+import type { AppProps } from 'next/app';
+
+function CustomApp({ Component, pageProps }: AppProps) {
+	return (
+		<>
+			<Head>
+				<title>Micro Frontend with Module Federation</title>
+			</Head>
+			<main>
+				<Component {...pageProps} />
+			</main>
+		</>
+	);
+}
+
+export default CustomApp;
